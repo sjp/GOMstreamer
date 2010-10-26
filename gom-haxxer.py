10409f2f6fbe537fb937eea9025fcc15508cb3a7
Tomas Herman <tomas.herman@gmail.com>
2010-10-26 16:01:38 +0200

    added licence, encoding declared

--- gom-haxxer.py
+++ gom-haxxer.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+'''
+Copyright 2010 Tomáš Heřman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 import urllib2
 import cookielib
 import urllib
@@ -25,7 +41,7 @@ def main():
 		parser.set_defaults(command=defaultOSXcommandVLC)
 	else:
 		parser.set_defaults(command=defaultLinuxVLC)
-	parser.set_defaults(cache=1)
+	parser.set_defaults(cache=30000)
 	(options,args) = parser.parse_args()
 
 	gomtvURL = "http://www.gomtv.net"
@@ -37,7 +53,6 @@ def main():
 			   'mb_password': options.password,
 			  }
 	data = urllib.urlencode(values)
-	virtualFile = StringIO.StringIO("")
 	cookiejar = cookielib.LWPCookieJar()
 
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+EMAIL="youremail@mail.com"
+PASSWORD="YOURPASSWORD"
+python ./gom-haxxer.py "-e $EMAIL" "-p PASSWORD"
