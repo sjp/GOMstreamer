33b40cdfbc600a29b021e6245a5aab597ccd6666
Joseph McCullough <joseph@vertstudios.com>
2012-04-19 09:44:42 -0500

    Prompt for passwords by default on OSX/linux

--- linux/scheduled-play.sh
+++ linux/scheduled-play.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 ####################
+# If you're comfortable with your password being viewable in plaintext, you may
+# create a PASSWORD variable with your password in it. By default, the script
+# will prompt you for your password at runtime.
 EMAIL='youremail@example.com'
-PASSWORD='PASSWORD'
 QUALITY='SQTest'
 MODE='scheduled-play'
 KST='18:00'
 STREAM='both'
 ####################
 PARENT_DIR="$(dirname "$( cd "$( dirname "$0" )" && pwd )")"
+
+# If password not set in variable, get password from user at runtime.
+if [[ "$PASSWORD" = "" ]];
+then
+  # Turn off echo so password doesn't show up when typing.
+	stty -echo
+	read -p "GomTV.net Password: " PASSWORD
+  # Turn echo back on
+	stty echo
+	echo ""
+fi
 python "$PARENT_DIR/gomstreamer.py" -e $EMAIL -p $PASSWORD -q $QUALITY -m $MODE -t $KST -s $STREAM $*
--- mac/play.command
+++ mac/play.command
@@ -1,9 +1,24 @@
 #!/bin/sh
 ####################
+# If you're comfortable with your password being viewable in plaintext, you may
+# create a PASSWORD variable with your password in it. By default, the script
+# will prompt you for your password at runtime.
 EMAIL='youremail@example.com'
-PASSWORD='PASSWORD'
 QUALITY='SQTest'
 STREAM='both'
 ####################
 PARENT_DIR="$(dirname "$( cd "$( dirname "$0" )" && pwd )")"
+
+# If password not set in variable, get password from user at runtime.
+if [[ "$PASSWORD" = "" ]];
+then
+  # Turn off echo so password doesn't show up when typing.
+	stty -echo
+	read -p "GomTV.net Password: " PASSWORD
+  # Turn echo back on
+	stty echo
+	echo ""
+fi
+
 python "$PARENT_DIR/gomstreamer.py" -e $EMAIL -p $PASSWORD -q $QUALITY -s $STREAM $*
+
